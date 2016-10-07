1b2891e66ff9e83746834263953c03caea0c10cf
Mikael Håkansson <wmmihaa@hotmail.com>
2016-10-07 16:17:30 +0200

    Update hol4-complete.js

--- completed/hol4-complete.js
+++ completed/hol4-complete.js
@@ -1,66 +1,73 @@
 
 var expect = require('chai').expect;
 var request = require('request');
-require('../hol3-complete.js');
-
-var options = {
-    headers: { "Content-Type": "application/json"},
-    uri: 'http://localhost:8080/products',
-    method: undefined,
-    json: {}
-};
+require('../hol3.js');
 
 describe("Get all products", function () {
-    var result;
+    var result; // Used to store the result
+    // First we call the service
     before(function (done) {
-        options.method = "GET";
+        // Configure the call with content-type and uri
+        var options = {
+            headers: { "Content-Type": "application/json"},
+            uri: 'http://localhost:8080/products',
+            json: {}
+        };
+        // Make call
         request.get(options, function (err, res, body) {
             result = {err, res, body};
             done();
         });
         
     });
-    it('should execute without errors', function () {
+    it('should execute without errors', function (done) {
        expect(result.err).to.equal(null);
+       done();
     });
-    it('should return an http status 200', function () {
+    it('should return an http status 200', function (done) {
        expect(result.res.statusCode).to.equal(200);
+       done();
     });
-    it('should return three items', function () {
+    it('should return three items', function (done) {
        expect(result.body.length).to.equal(3);
+       done();
     });
 });
 describe("Get one product", function () {
     var result;
     
     before(function (done) {
-        options.method = "GET";
-        options.uri += "?id=3";
-        request.get(options, function (err, res, body) {
+        request.get('http://localhost:8080/products?id=3', function (err, res, body) {
             result = {err, res, body};   
             done();
         });
         
     });
-    it('should execute without errors', function () {
-       expect(result.err).to.equal(null);
+    it('should execute without errors', function (done) {
+       expect(result.err).to.equal(null);  
+       done();
     });
-    it('should return an http status 200', function () {
+    it('should return an http status 200', function (done) {
        expect(result.res.statusCode).to.equal(200);
+       done();
     });
-    it('should return "Dajm"', function () {
+    it('should return "Dajm"', function (done) {
        expect(result.body[0].name).to.equal('Dajm');
+       done();
     });
 });
 describe("Add one product", function () {
     var result;
     
     before(function (done) {
-        options.method = "POST";
-        options.json = {
-            id: 4,
-            name: "Fransk norgat",
-            price: 8.2
+        var options = {
+            headers: { "Content-Type": "application/json"},
+            uri: 'http://localhost:8080/products',
+            json: {
+                id: 4,
+                name: "Fransk norgat",
+                price: 8.2
+            }
         };
         request.post(options, function (err, res, body) {
             result = {err, res, body};    
@@ -68,32 +75,35 @@ describe("Add one product", function () {
         });
         
     });
-    it('should execute without errors', function () {
+    it('should execute without errors', function (done) {
        expect(result.err).to.equal(null);
+       done();
     });
-    it('should return an http status 200', function () {
+    it('should return an http status 200', function (done) {
        expect(result.res.statusCode).to.equal(200);
+       done();
     });
 });
 describe("Get all products again", function () {
     var result;
     
     before(function (done) {
-        options.method = "GET";
-        options.uri = 'http://localhost:8080/products';
-        request.get(options, function (err, res, body) {
+        request.get('http://localhost:8080/products', function (err, res, body) {
             result = {err, res, body};  
             done();
         });
         
     });
-    it('should execute without errors', function () {
+    it('should execute without errors', function (done) {
        expect(result.err).to.equal(null);
+       done();
     });
-    it('should return an http status 200', function () {
+    it('should return an http status 200', function (done) {
        expect(result.res.statusCode).to.equal(200);
+       done();
     });
-    it('should return four items', function () {
+    it('should return four items', function (done) {
        expect(result.body.length).to.equal(4);
+       done();
     });
 });
